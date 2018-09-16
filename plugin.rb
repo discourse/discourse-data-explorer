316923f1906d811d8547cea99bde8e44d58dd15e
Rishabh Nambiar <5862206+rishabhnambiar@users.noreply.github.com>
2018-09-16 21:58:16 +0530

    FEATURE: Show the number of results after running a query (limit 250)

--- plugin.rb
+++ plugin.rb
@@ -939,7 +939,7 @@ SQL
 
     def create
       # guardian.ensure_can_create_explorer_query!
-      
+
       query = DataExplorer::Query.from_hash params.require(:query)
       query.created_at = Time.now
       query.created_by = current_user.id.to_s
@@ -1050,6 +1050,7 @@ SQL
               success: true,
               errors: [],
               duration: (result[:duration_secs].to_f * 1000).round(1),
+              result_count: pg_result.values.length || 0,
               params: query_params,
               columns: cols,
             }
