493febd864c4c6b1cd1d691893ac43a6b9a5903c
Ricky Chon <rickychon99@gmail.com>
2020-07-24 13:43:55 -0400

    DEV: Favor require - load files only if they are not already loaded

--- plugin.rb
+++ plugin.rb
@@ -8,7 +8,7 @@
 
 enabled_site_setting :data_explorer_enabled
 
-load File.expand_path('../lib/discourse_data_explorer/engine.rb', __FILE__)
+require File.expand_path('../lib/discourse_data_explorer/engine.rb', __FILE__)
 register_asset 'stylesheets/explorer.scss'
 
 if respond_to?(:register_svg_icon)
