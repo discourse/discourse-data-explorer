332ae394e868f7f9901ea84ef9c8d26be15efda6
Isaac Janzen <50783505+janzenisaac@users.noreply.github.com>
2023-02-06 11:20:03 -0600

    DEV: Upgrade `group-reports` to Octane (#218)

--- assets/javascripts/discourse/controllers/group-reports-index.js
+++ assets/javascripts/discourse/controllers/group-reports-index.js
@@ -1,6 +1,3 @@
 import Controller from "@ember/controller";
-import { alias } from "@ember/object/computed";
 
-export default Controller.extend({
-  queries: alias("model.queries"),
-});
+export default class GroupReportsIndexController extends Controller {}
--- assets/javascripts/discourse/routes/group-reports-show.js
+++ assets/javascripts/discourse/routes/group-reports-show.js
@@ -2,18 +2,15 @@ import { ajax } from "discourse/lib/ajax";
 import DiscourseRoute from "discourse/routes/discourse";
 import { action } from "@ember/object";
 
-export default DiscourseRoute.extend({
-  controllerName: "group-reports-show",
-
+export default class GroupReportsShowRoute extends DiscourseRoute {
   model(params) {
     const group = this.modelFor("group");
     return ajax(`/g/${group.name}/reports/${params.query_id}`)
       .then((response) => {
-        let query = response.query;
-        let queryGroup = response.query_group;
+        const query = response.query;
+        const queryGroup = response.query_group;
 
         const queryParamInfo = query.param_info;
-
         const queryParams = queryParamInfo.reduce((acc, param) => {
           acc[param.identifier] = param.default;
           return acc;
@@ -28,15 +25,15 @@ export default DiscourseRoute.extend({
       .catch(() => {
         this.transitionTo("group.members", group);
       });
-  },
+  }
 
   setupController(controller, model) {
     controller.setProperties(model);
-  },
+  }
 
   @action
   refreshModel() {
     this.refresh();
     return false;
-  },
-});
+  }
+}
