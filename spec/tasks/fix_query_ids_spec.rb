0f6b30c2d84d44978be1c845267609446ca9dbf2
Bianca Nenciu <nbianca@users.noreply.github.com>
2023-02-23 19:10:46 +0200

    Revert "DEV: Update plugin to match latest guidelines (#227)" (#228)
    This reverts commit 9210cf6fa26f0f74e712239eeb199d7d12186766.

--- spec/tasks/fix_query_ids_spec.rb
+++ spec/tasks/fix_query_ids_spec.rb
@@ -4,8 +4,8 @@ require "rails_helper"
 
 describe "fix query ids rake task" do
   before do
+    Rake::Task.clear
     Discourse::Application.load_tasks
-    Rake::Task.tasks.map(&:reenable)
   end
 
   let(:query_name) { "Awesome query" }
@@ -117,7 +117,7 @@ describe "fix query ids rake task" do
     key = "q:#{id}"
 
     PluginStore.set(
-      DataExplorer::PLUGIN_NAME,
+      DataExplorer.plugin_name,
       key,
       attributes(name).merge(group_ids: group_ids, id: id),
     )
