d8fb9649192c9f083e366118aa757c6ef48072c1
Nat <natalie.tay@discourse.org>
2023-03-24 11:06:07 +0800

    Fix stree lint

--- spec/automation/recurring_data_explorer_result_pm_spec.rb
+++ spec/automation/recurring_data_explorer_result_pm_spec.rb
@@ -16,7 +16,9 @@ describe "RecurringDataExplorerResultPm" do
   fab!(:not_allowed_user) { Fabricate(:user) }
   fab!(:group) { Fabricate(:group, users: [user, another_user]) }
   fab!(:another_group) { Fabricate(:group, users: [group_user]) }
-  let!(:recipients) { [user.username, not_allowed_user.username, another_user.username, another_group.name] }
+  let!(:recipients) do
+    [user.username, not_allowed_user.username, another_user.username, another_group.name]
+  end
 
   before do
     SiteSetting.data_explorer_enabled = true
@@ -45,9 +47,7 @@ describe "RecurringDataExplorerResultPm" do
   context "when using recurring trigger" do
     it "sends the pm at recurring date_date" do
       freeze_time 1.day.from_now do
-        expect {
-          Jobs::DiscourseAutomationTracker.new.execute
-        }.to change { Topic.count }.by(3)
+        expect { Jobs::DiscourseAutomationTracker.new.execute }.to change { Topic.count }.by(3)
 
         title = "Scheduled Report for #{@query.name}"
         expect(Topic.last(3).pluck(:title)).to eq([title, title, title])
@@ -61,19 +61,25 @@ describe "RecurringDataExplorerResultPm" do
       end.to change { Topic.count }.by(3)
 
       created_topics = Topic.last(3)
-      expect(created_topics.pluck(:archetype)).to eq([Archetype.private_message, Archetype.private_message, Archetype.private_message])
-      expect(created_topics.map { |t| t.allowed_users.pluck(:username) }).to match_array([
-        [user.username, Discourse.system_user.username],
-        [another_user.username, Discourse.system_user.username],
-        [group_user.username, Discourse.system_user.username],
-      ])
+      expect(created_topics.pluck(:archetype)).to eq(
+        [Archetype.private_message, Archetype.private_message, Archetype.private_message],
+      )
+      expect(created_topics.map { |t| t.allowed_users.pluck(:username) }).to match_array(
+        [
+          [user.username, Discourse.system_user.username],
+          [another_user.username, Discourse.system_user.username],
+          [group_user.username, Discourse.system_user.username],
+        ],
+      )
     end
 
     it "has appropriate content from the report generator" do
       automation.update(last_updated_by_id: admin.id)
       automation.trigger!
 
-      expect(Post.last.raw).to include("Hi #{group_user.username}, your data explorer report is ready.\n\nQuery Name:\nUser Participation Statistics")
+      expect(Post.last.raw).to include(
+        "Hi #{group_user.username}, your data explorer report is ready.\n\nQuery Name:\nUser Participation Statistics",
+      )
     end
   end
 end
